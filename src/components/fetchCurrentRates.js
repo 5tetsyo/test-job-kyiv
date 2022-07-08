d35eb8d34227863273c06f68568357340dc6865d
5tetsyo <103964470+5tetsyo@users.noreply.github.com>
2022-07-08 13:52:03 +0300

    Delete src/components directory

--- src/components/fetchCurrentRates.js
+++
@@ -1,7 +0,0 @@
-export const getRates = (setRates) => {
-    const options = {method: 'GET', headers: {Accept: 'application/json'}};
-    fetch('https://api.fastforex.io/fetch-multi?from=UAH&to=EUR,GBP,USD,PLN&api_key=94182c3bbe-3a4f72674a-ren2bp', options)
-      .then(response => response.json())
-      .then(response => setRates(response.results))
-      .catch(err => console.error(err));
-}
